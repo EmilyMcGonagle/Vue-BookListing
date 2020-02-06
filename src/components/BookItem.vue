8f831f1d0a51100ff35daff1ea04ccbddfabfb82
Emily <bh47eo@student.sunderland.ac.uk>
2020-02-06 13:52:30 +0000

    Added book item component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <li v-for='book in books'>{{book.title}}:((book.author))</li>
+</template>
+    
+
+<script>
+export default {
+    data: () => {
+        name: 'BookItem';
+        props: [
+            'book',
+        ]
+    }
+}
+</script>
+
+<style>
+
+</style>
